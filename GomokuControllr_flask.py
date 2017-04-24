2a66e6484079743d2da0f0eb96166750978ec9f3
pjo901018 <pjo901018@korea.ac.kr>
2017-04-24 14:19:10 +0900

    소켓 서버 완성

--- GomokuControllr_flask.py
+++ GomokuControllr_flask.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "mysecret"
+socket_io = SocketIO(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello_world(name="Yangyi"):
+    name = request.args.get('name', name)
+    return 'Gomoku server {}!'.format(name)
 
+@app.route('/newgame')
+def newgame(name="Yangyi"):
+    name = request.args.get('name', name)
+    return render_template('newgame.html')
+
+@socket_io.on("message")
+def req(msg):
+    print("message : "+msg)
+
+    send(msg, broadcast=True)
 
 if __name__ == '__main__':
+    socket_io.run(app)
     app.run(debug=True, port=8001, host='0.0.0.0')
