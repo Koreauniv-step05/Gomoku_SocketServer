e3e7e41a7ab4fd462a7e2a603a865d4eb6a724b4
pjo901018 <pjo901018@korea.ac.kr>
2017-04-24 22:00:05 +0900

    yourturn thread까지 제작

--- GomokuControllr_flask.py
+++ GomokuControllr_flask.py
@@ -1,5 +1,9 @@
-from flask import Flask, request, render_template
-from flask_socketio import SocketIO, send
+import threading
+
+import time
+from flask import Flask, request, render_template, copy_current_request_context
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 
@@ -11,17 +15,38 @@ def hello_world(name="Yangyi"):
     name = request.args.get('name', name)
     return 'Gomoku server {}!'.format(name)
 
-@app.route('/newgame')
-def newgame(name="Yangyi"):
-    name = request.args.get('name', name)
-    return render_template('newgame.html')
+# @app.route('/newgame')
+# def newgame(name="Yangyi"):
+#     name = request.args.get('name', name)
+#
+#     return render_template('newgame.html')
 
-@socket_io.on("message")
-def req(msg):
-    print("message : "+msg)
+@socket_io.on("game_command")
+def game_command(cmd):
+    @copy_current_request_context
+    def send_yourturn():
+        while True:
+            print('emit game_broadcast')
+            emit('game_broadcast', 'yourturn')
+            time.sleep(10)
 
-    send(msg, broadcast=True)
+    if cmd == 'new_game':
+        # todo newgame
+        print("new_game : ")
+
+        # todo remove
+        thread = threading.Thread(target=send_yourturn)
+        thread.start()
+        print("threading : start")
+
+
+@socket_io.on("new_stone")
+def new_stone(newStonePoint):
+    # todo newstone
+    print("new_stone : "+newStonePoint)
 
 if __name__ == '__main__':
     socket_io.run(app)
     app.run(debug=True, port=8001, host='0.0.0.0')
+
+
---
+++ broadcaster.py
@@ -0,0 +1,4 @@
+import time
+
+from flask import copy_current_request_context
+from flask_socketio import emit
