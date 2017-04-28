7398d7aef45254a87c53aade0a84a59e8eb97e85
pjo901018 <kujyp@24dcaea7d349>
2017-04-28 14:43:06 +0000

    Adding flask_app.py to deply on pythonanywhere

---
+++ flask_app.py
@@ -0,0 +1,20 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+from server.SocketController import MyCustomNamespace
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def newgame():
+    return render_template("newgame.html")
+
+@app.route('/gui')
+def gui():
+    return render_template("gui.html")
+
+socketio = SocketIO(app)
+socketio.on_namespace(MyCustomNamespace())
