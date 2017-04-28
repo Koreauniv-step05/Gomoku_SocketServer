9cb54755662a006af958960c4ba242bf7a93f2e2
pjo901018 <pjo901018@korea.ac.kr>
2017-04-29 06:01:44 +0900

    메인파일 주석정리

--- GomokuController_flask.py
+++ GomokuController_flask.py
@@ -13,34 +13,12 @@ mGameController = None
 
 @app.route('/')
 def hello_world(name="Yangyi"):
-    # name = request.args.get('name', name)
     return render_template("newgame.html")
 
 @app.route('/gui')
 def gui(name="Yangyi"):
-    # name = request.args.get('name', name)
     return render_template("gui.html")
-#
-# @socket_io.on("connect")
-# def on_connect():
-#     print('connect')
-#     emit('user_connect', 'user_connect: reply')
-#     emit('user_connect', 'user_connect: broadcast', broadcast=True)
-#
-# @socket_io.on("disconnect")
-# def on_disconnect():
-#     print('disconnect')
-#     emit('user_disconnect', 'user_disconnect: reply')
-#     emit('user_disconnect', 'user_disconnect: broadcast', broadcast=True)
-#
-# @socket_io.on("reconnect")
-# def on_reconnect():
-#     print('reconnect')
-#     emit('user_reconnect', 'user_reconnect: reply')
-#     emit('user_reconnect', 'user_reconnect: broadcast', broadcast=True)
 
 if __name__ == '__main__':
     socket_io.on_namespace(MyCustomNamespace())
     socket_io.run(app, host='0.0.0.0', port=config.PORT, debug=True)
-
-    # app.run(debug=True, port=config.PORT, host='0.0.0.0')
